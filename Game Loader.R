318046b2fc24a253e7d45cd0efbbe683005346a9
StevenPlai <stevenrplai@gmail.com>
2021-08-31 01:57:33 -0500

    Update

---
+++ Game Loader.R
@@ -0,0 +1,3 @@
+library(cfbfastR)
+
+lines <- cfbd_betting_lines(year = 2021, week = 1)
--- Scraper.R
+++ Scraper.R
@@ -13,10 +13,24 @@ fei <- read_html("https://www.bcftoys.com/2021-pfei/") %>% html_table()
 fei <- fei[[1]] %>% row_to_names(row_number = 2, remove_rows_above = T) %>%
   clean_names() %>% 
   select(team,"rating" = fei,"o_rating" = ofei,"d_rating" = dfei) %>% 
-  filter(!is.na(team) & team!= "Team")
+  filter(!is.na(team) & team!= "Team") %>%
+  mutate(rating = standardize(as.numeric(rating)),
+         o_rating = standardize(as.numeric(o_rating)),
+         d_rating = standardize(as.numeric(d_rating)))
 
 sp <- read.csv("~/Downloads/spplus.csv") %>% 
   separate(col=Team, into = c("rk","team"), sep=" ", extra="merge") %>%
   select(team, "rating" = Rating, "o_rating" = Offense, "d_rating" = Defense) %>% 
   separate(col=o_rating, into="o_rating", sep=" ", extra="drop") %>% 
-  separate(col=d_rating, into="d_rating", sep=" ", extra="drop")
+  separate(col=d_rating, into="d_rating", sep=" ", extra="drop") %>%
+  mutate(rating = standardize(as.numeric(rating)),
+         o_rating = standardize(as.numeric(o_rating)),
+         d_rating = standardize(as.numeric(d_rating)))
+
+laz <- read_html("https://www.lazindex.com/ncaa.php") %>% html_table()
+
+laz <- laz[[1]] %>% filter(DIVISION == "FBS") %>% select("school" = SCHOOL,
+                                                         "rating" = POWER) %>%
+  mutate(rating = standardize(rating))
+
+
