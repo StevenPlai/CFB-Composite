f11f00b36c51e35dfd213fa6fdb2e33e38ec2d13
StevenPlai <stevenrplai@gmail.com>
2021-08-31 02:13:17 -0500

    Update Scraper.R

--- Scraper.R
+++ Scraper.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(rvest)
 library(janitor)
 library(robustHD)
+library(heatmaply)
 
 fpi <- read_html("https://www.espn.com/college-football/fpi") %>% html_table()
 
@@ -33,4 +34,59 @@ laz <- laz[[1]] %>% filter(DIVISION == "FBS") %>% select("school" = SCHOOL,
                                                          "rating" = POWER) %>%
   mutate(rating = standardize(rating))
 
+
+tr <- read_html("https://www.teamrankings.com/college-football/ranking/predictive-by-other") %>% 
+  html_table()
+
+tr <- tr[[1]] %>% 
+  separate(col=Team, into = c("team","x","y","z"), sep=" ", extra="merge") %>%
+  mutate(h1=grepl("^[A-Za-z-]+$", x),
+         h2=grepl("^[A-Za-z-]+$", y),
+         h3=if_else(h1==F,0,if_else(h2==F,1,2)),
+         team=if_else(h3==0,team,if_else(h3==1,paste0(team," ",x),paste0(team," ",x," ",y)))) %>% 
+  select(team,"rating" = Rating) %>% mutate(rating = standardize(as.numeric(rating)))
+
+vs <- read_html("https://www.versussportssimulator.com/FBS/rankings") %>% html_table()
+
+vs <- vs[[1]] %>% 
+  separate(col=School, into = c("team","x","y","z"), sep=" ", extra="merge") %>%
+  mutate(h1=grepl("^[A-Za-z-]+$", x),
+         h2=grepl("^[A-Za-z-]+$", y),
+         h3=if_else(h1==F,0,if_else(h2==F,1,2)),
+         team=if_else(h3==0,team,if_else(h3==1,paste0(team," ",x),paste0(team," ",x," ",y)))) %>% 
+  mutate(rating = log(Rating)) %>% 
+  select(team,rating) %>% mutate(rating = standardize(as.numeric(rating)))
+
+how <- read_html("http://www.jhowell.net/cf/cf2021.htm") %>% html_table()
+
+how <- how[[1]] %>% row_to_names(row_number = 1) %>%
+  select("team"=Team,"rating"=PR) %>% mutate(rating = standardize(as.numeric(rating)))
+
+fox <- read_html("http://www.statfox.com/cfb/power/") %>% html_table()
+
+fox <- fox[[4]] %>% row_to_names(row_number = 1) %>%
+  select("team"=Team,"rating"=Rating) %>% mutate(rating = standardize(as.numeric(rating)))
+
+dr <- read_html("https://www.dratings.com/sports/ncaa-fbs-football-ratings/") %>% html_table()
+
+dr <- dr[[1]] %>% select(Rank,"rating"=Overall) %>%
+  separate(col=Rank, into = c("rk","team"), sep="\\.", extra="merge") %>%
+  separate(col=team, into = c("team"), sep="\\(", extra="drop") %>% select(team,rating) %>%
+  mutate(team=trimws(team),
+         rating = standardize(as.numeric(rating)))
+
+argh <- read_html("http://www.arghratings.com/currate.html") %>% html_table()
+
+argh <- argh[[1]] %>% select("team"=Team,"rating"="Power Rating") %>%
+  mutate(rating = standardize(as.numeric(rating)))
+
+tpr <- read_html("https://thepowerrank.com/college-football/bowl-subdivision-rankings/") %>% html_table()
+
+tpr <- tpr[[1]] %>% select("team"=Team,"rating"=Rating) %>%
+  mutate(rating = standardize(as.numeric(rating)))
+
+pr <- read_html("https://piratings.wordpress.com/") %>% html_table()
+
+pr <- pr[[4]] %>% row_to_names(row_number = 1) %>% select("team"=Team,"rating"=Rating) %>%
+  mutate(rating = standardize(as.numeric(rating)))
 
