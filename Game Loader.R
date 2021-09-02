468879766191bc6c9d0de2816ce06f71d68d40fc
StevenPlai <stevenrplai@gmail.com>
2021-09-02 11:58:19 -0500

    Update Game Loader.R

--- Game Loader.R
+++ Game Loader.R
@@ -1,3 +1,5 @@
 library(cfbfastR)
 
-lines <- cfbd_betting_lines(year = 2021, week = 1)
+lines1 <- cfbd_betting_lines(year = 2021, week = 1)
+lines2 <- cfbd_betting_lines(year = 2021, week = 2)
+lines3 <- cfbd_betting_lines(year = 2021, week = 2)
